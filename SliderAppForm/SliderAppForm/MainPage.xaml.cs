6a08aa42870b0ff0b277f326d33ed0d4a7c7f458
Paul <paul.rey@sryther.fr>
2016-11-25 11:04:13 +0100

    Add in the MainPage the Slider and the binding to its value

--- SliderAppForm/SliderAppForm/MainPage.xaml.cs
+++ SliderAppForm/SliderAppForm/MainPage.xaml.cs
@@ -9,9 +9,42 @@ namespace SliderAppForm
 {
     public partial class MainPage : ContentPage
     {
+        public Slider MySlider = new Slider
+        {
+            Minimum = 0,
+            Maximum = 100,
+            Value = 0
+        };
+
+        public Label TitleLabel = new Label
+        {
+            HorizontalTextAlignment = TextAlignment.Center,
+            FontSize = 24,
+            Text = "Valeur du slider"
+        };
+
+        public Label ValueLabel = new Label
+        {
+            HorizontalTextAlignment = TextAlignment.Center,
+            FontSize = 24
+        };
+
         public MainPage()
         {
             InitializeComponent();
+
+            ValueLabel.SetBinding(Label.TextProperty, "Value");
+            ValueLabel.BindingContext = MySlider;
+
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                    TitleLabel,
+                    ValueLabel,
+                    MySlider
+                }
+            };
         }
-    }
+    } 
 }
